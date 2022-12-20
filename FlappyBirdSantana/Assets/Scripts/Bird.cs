1cd9d4e27fea99d09e1c281c3cc2768a6fbe94e5
Lindsayy121 <116232724+Lindsayy121@users.noreply.github.com>
2022-12-20 10:31:03 -0800

    Finished final
    Turned in

--- FlappyBirdSantana/Assets/Scripts/Bird.cs
+++ FlappyBirdSantana/Assets/Scripts/Bird.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
-    public float upForce;
-    private bool isDead = false;
+    public float upForce = 200f;
 
+    private bool isDead = false;
     private Animator anim;
     private Rigidbody2D rb2d;
 
@@ -14,10 +14,9 @@ public class Bird : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //Get reference to the Animator component attached to this GameObject.
+      
         anim = GetComponent<Animator> ();
-
-        rb2d = GetComponent<Rigidbody2D>();
+        rb2d = GetComponent<Rigidbody2D> ();
     }
 
     // Update is called once per frame
@@ -25,29 +24,21 @@ public class Bird : MonoBehaviour
     {
         if (isDead == false)
         {
-            //Look for input to trigger a "flap".
             if (Input.GetMouseButtonDown(0))
             {
                 //...tell the animator about it and then...
                 anim.SetTrigger("Flap");
-                //...zero out the birds current y velocity before...
                 rb2d.velocity = Vector2.zero;
-                //... new Vecter2(rb2d.velocity.x,0);
-                //..givng the bird some upward force.
-                rb2d.AddForce(new Vector2(0, upForce));
+                rb2d.AddForce(new Vector2 (0, upForce));
             }
         }
     }
 
     void OnCollisionEnter2D(Collision2D other)
     {
-        // zero out the bird velocity
         rb2d.velocity = Vector2.zero;
-        // If the bird collides with something set it to dead...
         isDead = true;
-        //...and tell the animaotr abouyt it...
         anim.SetTrigger("Die");
-        //...and tell the game control about it.
         GameControl.instance.BirdDied();
     }
 }
--- FlappyBirdSantana/Assets/Scripts/GameControl.cs
+++ FlappyBirdSantana/Assets/Scripts/GameControl.cs
@@ -1,55 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameControl : MonoBehaviour
 {
     public static GameControl instance;
-    public Text scoreText;
     public GameObject gameOvertext;
-
-    private int score = 0;
+    public TMP_Text scoreText;
     public bool gameOver = false;
     public float scrollSpeed = -1.5f;
+
+    private int score = 0;
 
     void Awake()
     {
         if (instance == null)
-
+        {
             instance = this;
 
+        }
         else if (instance != this)
-
+        {
             Destroy(gameObject);
+        }
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+   
     // Update is called once per frame
     void Update()
     {
-       if (gameOver && Input.getMouseButtonDown(0))
+       if (gameOver == true && Input.GetMouseButtonDown (0))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex);
         }
     }
 
-    public void BirdDied()
+    public void BirdScored()
     {
         if (gameOver)
+        {
             return;
-
+        }
         score++;
-
         scoreText.text = "Score: " + score.ToString();
     }
 
     public void BirdDied()
     {
-        gameOvertext.SetActive(true);
+        gameOvertext.SetActive (true);
         gameOver = true;
     }
 }
